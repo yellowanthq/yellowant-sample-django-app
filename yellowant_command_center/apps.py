9f3f922aa795e8b2a9845783516033af62a979ba
Manik Singh <manik@yellowant.com>
2018-04-02 16:03:27 +0530

    delete unwanted files

--- yellowant_command_center/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YellowantCommandCenterConfig(AppConfig):
-    name = 'yellowant_command_center'
--- yellowant_message_builder/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YellowantMessageBuilderConfig(AppConfig):
-    name = 'yellowant_message_builder'
