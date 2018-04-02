7ea72be14eef233e882658838eeea77d5702a4a4
Manik Singh <manik@yellowant.com>
2018-04-02 16:07:41 +0530

    fix urls

--- yellowant_todoapp/urls.py
+++ yellowant_todoapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
-from todo import urls as todo_urls
 from web import urls as web_urls
 from yellowant_api import urls as yellowant_api_urls
 
@@ -12,5 +11,4 @@ urlpatterns = [
     path('', include(yellowant_api_urls)),
     path("accounts/", include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
-    path("api/", include(todo_urls)),
 ]
