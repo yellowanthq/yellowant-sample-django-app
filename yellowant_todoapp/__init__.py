fe9d66d5119222733430ee045e2b8860d27127b1
Manik Singh <manik@yellowant.com>
2018-05-25 19:42:41 +0530

    update init file for heroku env

--- yellowant_todoapp/__init__.py
+++ yellowant_todoapp/__init__.py
@@ -1,13 +1,16 @@
 import os
 
-
-# fetch environment variables
-YA_DEVELOPER_TOKEN = os.environ.get("YA_DEVELOPER_TOKEN")
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-
-# set website
-website = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
+DJANGO_ENV= os.environ.get("DJANGO_ENV", "heroku")
 
 
-os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
-os.system('yellowant sync -q --api_url {}yellowantauthurl/yellowantauthurl/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}redirecturl/yellowantredirecturl/'.format(website,website,website,website,website))
+if DJANGO_ENV == "heroku":
+    # fetch environment variables
+    YA_DEVELOPER_TOKEN = os.environ.get("YA_DEVELOPER_TOKEN")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+
+    # set website
+    website = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
+
+
+    os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
+    os.system('yellowant sync -q --api_url {}yellowantauthurl/yellowantauthurl/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}redirecturl/yellowantredirecturl/'.format(website,website,website,website,website))
