bbcd85251dda4ddd43edc842b422d0958a390477
Manik Singh <manik@yellowant.com>
2018-05-25 19:42:04 +0530

    add new signup view

--- web/views.py
+++ web/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 from yellowant_api.models import UserIntegration
 
@@ -15,3 +17,19 @@ def index(request):
             context["user_integrations"].append(user_integration)
 
     return render(request, "home.html", context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        print("invalid")
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
