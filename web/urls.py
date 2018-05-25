1b9fd1adf2b3c75d4aecfea45199fd12ae656836
Manik Singh <manik@yellowant.com>
2018-05-25 19:42:59 +0530

    register signup url

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from web.views import index
+from web.views import index, signup
 
 
 urlpatterns = [
     path("", index, name="home"),
+    path("signup/", signup, name="signup")
 ]
