aa9876da738391ce049f1131837af91a3905d29f
Manik Singh <manik@yellowant.com>
2018-05-25 20:09:12 +0530

    fix inline command params

--- yellowant_todoapp/__init__.py
+++ yellowant_todoapp/__init__.py
@@ -13,6 +13,5 @@ if DJANGO_ENV == "heroku":
 
 
     os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
-    os.system('yellowant sync -q --api_url {}yellowantauthurl/yellowantauthurl/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}redirecturl/yellowantredirecturl/'.format(website,website,website,website,website))
+    os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}yellowant-oauth-redirect/'.format(website,website,website,website,website))
     os.system("ls -al")
-    
