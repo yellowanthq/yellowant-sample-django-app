36756159204049153632b89b847c4dadf7092f5f
Manik Singh <manik@yellowant.com>
2018-05-28 14:51:10 +0530

    lint todo models

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,15 @@
+"""Sample models for the Todo list application"""
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
+    """Todo Item
+
+    Attributes:
+        title (str): Header title to summarize the todo item.
+        description (int): Detailed description of the todo item.
+    """
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
@@ -10,8 +17,14 @@ class Todo(models.Model):
         return self.title
 
 class UserTodo(models.Model):
+    """Todo Owner Relationship
+
+    Attributes:
+        user (User): Owner of the todo item.
+        todo (Todo): The todo item.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user.username} - {self.todo.title}"
+        return "{} - {}".format(self.user.username, self.todo.title) #pylint: disable=no-member
