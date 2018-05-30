a2350916d7afe562ed4fa8dab7d1c0f41fe9f4b8
Manik Singh <manik@yellowant.com>
2018-05-30 18:40:49 +0530

    lint yellowant_message_builder attachments

--- yellowant_message_builder/attachments.py
+++ yellowant_message_builder/attachments.py
@@ -1,10 +1,20 @@
-### YA Message Attachment Builders ###
+""" YA Message Attachment Builders """
 from yellowant.messageformat import MessageAttachmentsClass, AttachmentFieldsClass
 
 from .buttons import update_item_button
 
 
 def item_attachment(item, user_integration, attachment=None):
+    """YellowAnt message attachment to show an item's details.
+
+    Args:
+        item (dict): Item data.
+        user_integration (yellowant_api.models.UserIntegration): User integration data.
+        attachment (MessageAttachmentsClass, optional): Attachment to add details to.
+
+    Returns:
+        MessageAttachmentsClass: An attachment with the todo details and actions to update it.
+    """
     attachment = attachment or MessageAttachmentsClass()
     attachment.title = item.get("title")
     attachment.text = item.get("description")
