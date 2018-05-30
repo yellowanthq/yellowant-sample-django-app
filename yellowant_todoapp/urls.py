28da0c04ac838d093aaf147c526053e9e3da74d4
Manik Singh <manik@yellowant.com>
2018-05-30 18:48:04 +0530

    lint yellowant_todoapp urls

--- yellowant_todoapp/urls.py
+++ yellowant_todoapp/urls.py
@@ -1,7 +1,6 @@
 """yellowant_todoapp URL Configuration"""
 from django.urls import include, path
 from django.contrib import admin
-from django.views.generic.base import TemplateView
 
 from web import urls as web_urls
 from yellowant_api import urls as yellowant_api_urls
