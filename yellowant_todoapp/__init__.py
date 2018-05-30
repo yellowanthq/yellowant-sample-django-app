60e78c83265fc5ccad42f6de75bfa1ffa0b26583
Manik Singh <manik@yellowant.com>
2018-05-30 18:47:27 +0530

    fix init of yellowant_todoapp

--- yellowant_todoapp/__init__.py
+++ yellowant_todoapp/__init__.py
@@ -1,6 +1,7 @@
+"""Initialize YellowAnt Sample Todo Application"""
 import os
 
-DJANGO_ENV= os.environ.get("DJANGO_ENV", "heroku")
+DJANGO_ENV = os.environ.get("DJANGO_ENV", "heroku")
 
 
 if DJANGO_ENV == "heroku":
@@ -9,9 +10,12 @@ if DJANGO_ENV == "heroku":
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
 
     # set website
-    website = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
+    WEBSITE = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
 
 
-    os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
-    os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}yellowant-oauth-redirect/'.format(website,website,website,website,website))
+    os.system("yellowant auth --token {} --host https://www.yellowant.com "\
+        .format(YA_DEVELOPER_TOKEN))
+    os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} \
+        --privacy_policy_url {}privacy --redirect_uris {}yellowant-oauth-redirect/'\
+        .format(WEBSITE, WEBSITE, WEBSITE, WEBSITE, WEBSITE))
     os.system("ls -al")
