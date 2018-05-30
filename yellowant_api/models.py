4020a5d78550a3b8a8375fc9c90fc6c23d9dc352
Manik Singh <manik@yellowant.com>
2018-05-30 18:19:54 +0530

    lint yellowant_api models

--- yellowant_api/models.py
+++ yellowant_api/models.py
@@ -1,3 +1,4 @@
+"""Models for saving YellowAnt user details and application user details"""
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,27 +7,28 @@ class UserIntegration(models.Model):
     """User YellowAnt Integration model
 
     Holds the information which identifies your user with an integration on YA.
-    
-    Since a single YA user is allowed to have multiple integrations with your application on YA, you need to store
-    a one-to-many relationship for a user to many YA user integrations.
-    
-    For example, if this is a mail application, users might want to connect their personal mail and work mail with YA.
-    In this case, a single user will have two YA integrations, one which connects the personal mail, and the other which
-    connects the work mail.
+
+    Since a single YA user is allowed to have multiple integrations with your application on YA, you
+    need to store a one-to-many relationship for a user to many YA user integrations.
+
+    For example, if this is a mail application, users might want to connect their personal mail and
+    work mail with YA. In this case, a single user will have two YA integrations, one which connects
+    the personal mail, and the other which connects the work mail.
 
     Fields:
         user (User): Your application user
         yellowant_user_id (int): YA user id
-        yellowant_team_subdomain (str): YA user's team subdomain # each user on YA belongs to a team, irrespective of
-            the team size
+        yellowant_team_subdomain (str): YA user's team subdomain # each user on YA belongs to a
+        team, irrespective of the team size
         yellowant_integration_id (int): Unique YA user integration id
-        yellowant_integration_invoke_name (str): YA integration invoke name # each integration of your application is
-            controlled by the user with the help of your application's default invoke name. Since a YA user is allowed
-            to have multiple integrations with your application, YA will suffix the default invoke name for users who
-            want to integrate more than once with your application, so that they can control the different integrations
+        yellowant_integration_invoke_name (str): YA integration invoke name # each integration of
+            your application is controlled by the user with the help of your application's default
+            invoke name. Since a YA user is allowed to have multiple integrations with your
+            application, YA will suffix the default invoke name for users who want to integrate more
+            than once with your application, so that they can control the different integrations
             with their respective invoke names.
-        yellowant_integration_token (str): Unique token per integration # This token allows your application to 
-            perform actions on the YA platform for the YA user integration.
+        yellowant_integration_token (str): Unique token per integration # This token allows your
+            application to perform actions on the YA platform for the YA user integration.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     yellowant_user_id = models.IntegerField(null=False)
@@ -38,7 +40,7 @@ class UserIntegration(models.Model):
 
 class YellowAntRedirectState(models.Model):
     """Model to store YA oauth requests with users
-    
+
     Create a new entry between the user and the oauth state
 
     Fields:
